9f380797aacfc9b146bd470e923cddec2b2cb94b
jverkhhaskell <jverkhhaskell@pugetsound.edu>
2024-02-12 02:30:47 +0000

    Please commit

--- ls2.c
+++ ls2.c
@@ -7,12 +7,85 @@
 #include "ls2.h"
 
 
+
 // TODO: function definitions here for ls2
 
-/**
- * This is just an example. Delete this before 
- * submission.
- */
-void example(int* x) {
-    *x = thisIsGlobal;
-} 
+void printDir(const char *dirpath, int indent) {
+    char filepath[128];
+    DIR *p;
+    struct dirent *d;
+    struct stat fileInfo;
+    p=opendir(dirpath);
+    if(p == NULL) {
+        printf("You requested a directory that isn't real, go away\n");
+        return 0;
+    }
+    while(d=readdir(p)) {
+        strcpy(filepath, dirpath);
+        strcat(filepath, "/");
+        strcat(filepath, d->d_name);
+        lstat(filepath, &fileInfo);
+        if (S_ISDIR(fileInfo.st_mode)) {
+            if(strcmp(d->d_name, ".") == 0 || strcmp(d->d_name, "..") == 0);
+            else {
+                for(int i = 0; i < indent; i++) {
+                    printf("    ");
+                }
+                printf("%s%s",d->d_name, "/ (directory)\n");
+                printDir(filepath, indent + 1);
+            }
+        } 
+        else if (S_ISREG(fileInfo.st_mode)) {
+            for(int i = 0; i < indent; i++) {
+                printf("    ");
+            }
+            printf("%s (%d bytes)\n",d->d_name, fileInfo.st_size);
+        }       
+    }
+}
+
+int searchDir(const char *dirpath, int indent, const char *name, stack_t *s) {
+    DIR *p;
+    struct dirent *d;
+    struct stat fileInfo;
+    int corFile = 0;
+    p=opendir(dirpath);
+    while(d=readdir(p)) {
+        char *filepath = (char*) malloc(128);    
+        filepath[0] = '\0';  //empty the string
+        strcpy(filepath, dirpath);
+        strcat(filepath, "/");
+        strcat(filepath, d->d_name);
+        lstat(filepath, &fileInfo);
+        if (S_ISDIR(fileInfo.st_mode)) {
+            if(strcmp(d->d_name, ".") == 0 || strcmp(d->d_name, "..") == 0);
+            else {
+                if(searchDir(filepath, indent + 1, name, s)) {
+                    //printf("matching file found within directory %s\n", d->d_name);
+                    char *filename = (char*) malloc(128);    
+                    filename[0] = '\0';  //empty the string
+                    for(int i = 0; i < indent; i++) {
+                        snprintf(filename + strlen(filename), 128, "    ");
+                    }
+                    snprintf(filename + strlen(filename), 128, "%s%s",d->d_name, "/ (directory)");
+                    push(s, filename);
+                    corFile = 1;
+                }
+            }
+        } 
+        else if (S_ISREG(fileInfo.st_mode)) {
+            if(!strcmp(d->d_name, name)) {
+                char *filename = (char*) malloc(128);    
+                filename[0] = '\0';  //empty the string
+                for(int i = 0; i < indent; i++) {
+                    snprintf(filename + strlen(filename), 128, "    ");
+                }
+                snprintf(filename + strlen(filename), 128, "%s (%d bytes)",d->d_name, fileInfo.st_size);
+                push(s, filename);                               
+                corFile = 1;
+            }
+        }    
+        free(filepath);
+    }
+return corFile; 
+}
--- main.c
+++ main.c
@@ -9,16 +9,23 @@
  */
 int main(int argc, char* argv[]) {
 	// stack stores the lines to print out
-	stack_t *s = initstack();
+	stack_t *s = initstack(); // freestack isn't freeing this, so valgrind says I have a memory leak, I don't know what to do with this
+	if(argc == 2) {
+		printf("Requested search of directory: %s\n", argv[1]);
+		printDir(argv[1], 0);
+	} else if(argc == 3) {
+		printf("requested search of directory: %s with name %s\n", argv[1], argv[2]);
+		searchDir(argv[1], 0, argv[2], s);
+	} else printf("Usage: ./ls2 <path> [exact-match-pattern]\n");
+	// printf("completed search");
+	// push(s, "Hello1");
+	// push(s, "Hello2");
+	// push(s, "Hello3");
 
-	push(s, "Hello1");
-	push(s, "Hello2");
-	push(s, "Hello3");
-
-	// print stack
+	// // print stack
 	printstack(s);
 
-	// free up stack
-	freestack(s);
+	// // free up stack
+	freestack(s); 
 	return 0;
 }
